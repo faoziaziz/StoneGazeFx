32cec46ade3b72610c728762d79180ebc9adcd52
azizfaozi <admin@labseni.com>
2018-12-10 17:23:10 +0700

    kakmu

--- src/main/java/com/herokuapp/faoziaziz/frontend/MainInterface.java
+++ src/main/java/com/herokuapp/faoziaziz/frontend/MainInterface.java
@@ -1,32 +1,73 @@
+/*
+*   Nama        : MainInterface.java
+*   Description : Ini class yang akan dipanggil sebagai user
+*                 interface utama.
+* */
 package com.herokuapp.faoziaziz.frontend;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class MainInterface{
-    JToolBar barAlat= new JToolBar("ALat KU");
-    JPanel PanelKiri = new JPanel();
-    JPanel PanelTengah = new JPanel();
-    ImageIcon gambarIcon = new ImageIcon("./gambar/icon", "ini asik");
-    JMenuBar menuBar=new JMenuBar();
+public class MainInterface {
 
+    JFrame frame;
+    Container contentPane ;
 
     public MainInterface() {
         FrameUtama();
 
     }
 
-    public void FrameUtama(){
+    private void FrameUtama(){
 
         /*
         *   Settingan awal frame Utama
         *
         * */
+
         JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame myWindow = new JFrame();
-        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myWindow.setTitle("Jendela Program");
-        myWindow.setSize(400, 200);
-        myWindow.setVisible(true);
+        this.frame=new JFrame("StoneGaze");
+        this.contentPane = this.frame.getContentPane();
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setSize(400, 300);
+        this.setPanelAtas();
+        this.setPanelKiri(); //untuk settingan panel kiri
+        this.setPanelKanan();
+        this.setPanelBawah();
+        this.setPanelTengah();
+        this.frame.pack();
+        this.frame.setVisible(true);
+    }
+    
+    private void setPanelAtas(){
+
+    }
+
+    private void setPanelKiri(){
+        /*
+        *   Settingan untuk panel kiri
+        *
+        * */
+
+        JPanel PanelKiri = new JPanel();
+        JButton TombolKiri1 = new JButton("Button 1");
+        JButton TombolKiri2 = new JButton("Button 2");
+        PanelKiri.add(TombolKiri1);
+        PanelKiri.add(TombolKiri2);
+
+        this.contentPane.add(PanelKiri, BorderLayout.WEST);
+    }
+
+    private void setPanelKanan(){
+
+    }
+
+    private void setPanelTengah(){
+
+    }
+
+    private void setPanelBawah(){
+
     }
 
 }
